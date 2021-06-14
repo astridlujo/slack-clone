11fced68a368b416bdf588ab88fce89d9d2b7753
Astrid Lujo <84393461+astridlujo@users.noreply.github.com>
2021-06-14 15:30:27 -0500

    Created the header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCjk1fhMsTVz_GQhEwZ0wvyTX2Ce9otXM",
+    authDomain: "slack-clone-5fc83.firebaseapp.com",
+    projectId: "slack-clone-5fc83",
+    storageBucket: "slack-clone-5fc83.appspot.com",
+    messagingSenderId: "854061602792",
+    appId: "1:854061602792:web:8c99b65e5c19f9e4e5765d"
+  };
